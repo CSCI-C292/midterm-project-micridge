a8446af89c6114d91d9550d95af76cc77aece9eb
micridge <micridge@iu.edu>
2020-11-05 19:05:46 -0500

    final commit

---
+++ Assets/GameState.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameState : MonoBehaviour
+{
+    int _score = 0;
+    bool hasKey = false;
+    int _sceneNum = 0;
+
+    [SerializeField] GameObject _scoreText;
+
+    public static GameState Instance;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        Instance = this;
+
+    }
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Submit")){
+            MenuStart();
+            MenuEnd();
+        }
+    }
+
+    public void IncreaseScore(int amount)
+    {
+        _score += amount;
+        _scoreText.GetComponent<Text>().text = "Score: " + _score;
+    }
+
+    public void KeyRetrieve()
+    {
+        hasKey = true;
+    }
+
+
+    public void KeyCheck()
+    {
+        if(hasKey == true){
+            hasKey = false;
+            _sceneNum = _sceneNum + 1;
+            SceneManager.LoadScene(_sceneNum, LoadSceneMode.Single);
+        }
+    }
+
+    public void MenuStart()
+    {
+        if(SceneManager.GetActiveScene().buildIndex == 0){
+            _sceneNum = _sceneNum + 1;
+            SceneManager.LoadScene(_sceneNum, LoadSceneMode.Single);
+        }
+    }
+
+    public void MenuEnd()
+    {
+        if(SceneManager.GetActiveScene().buildIndex == 11){
+            _score = 0;
+            _sceneNum = 0;
+            SceneManager.LoadScene(_sceneNum, LoadSceneMode.Single);
+            Destroy(gameObject);
+            Destroy(_scoreText);
+        }
+    }
+
+}
---
+++ Assets/player/TriangleGuy.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriangleGuy : MonoBehaviour
+{
+    [SerializeField] float _moveSpeed = 3f;
+    [SerializeField] float _moveSpeedUp = 3f;
+    [SerializeField] GameObject _projPrefab;
+    bool isGrounded;
+    bool isBig = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Fire();
+        Move();
+        SizeChange();
+    }
+
+    void FixedUpdate()
+    {
+        isGrounded = false;
+    }
+
+    void Move()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal") * _moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        if(Input.GetButtonDown("Jump") && isGrounded){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxis("Horizontal") * _moveSpeed, _moveSpeedUp);
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        isGrounded = true;
+    }
+
+    void Fire()
+    {
+        if(Input.GetButtonDown("Fire1")){
+            Instantiate(_projPrefab, new Vector3(transform.position.x, transform.position.y, -1), Quaternion.identity);
+        }
+    }
+
+    void SizeChange()
+    {
+        if(Input.GetButtonDown("Fire2")){
+            isBig = !isBig;
+            if(isBig){
+                _moveSpeedUp = _moveSpeedUp + 2;
+                transform.localScale = new Vector2(2, 2);
+            }
+            else{
+                _moveSpeedUp = _moveSpeedUp - 2;
+                transform.localScale = new Vector2(1, 1);
+            }
+        }
+
+    }
+
+
+}
